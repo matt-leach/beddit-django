7ee822dbde2901476c6c80eec26f8d37acbed5b2
Matt <matt.leach25@gmail.com>
2014-11-13 23:51:57 +0000

    playing around a bit with d3 graphing

--- beddit/sleeps/views.py
+++ beddit/sleeps/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse
 from pyBeddit.clients import BedditClient
 from django.shortcuts import redirect, render
 from django.conf import settings
+import json
+from datetime import datetime
+
+def convert_date_format(date_string):
+    """
+    converts YYYY-MM-DD to DD/MM
+    """
+    return datetime.strptime(date_string, '%Y-%m-%d').strftime('%d/%m')
+
 
 def index(request):
     
@@ -12,7 +21,9 @@ def index(request):
         sleep_data = b.get_sleep_scores()
         print sleep_data
         
-        context = {"data": sleep_data}
+        data = [{"date": convert_date_format(date), "score": score} for date, score in sleep_data.items()]
+        
+        context = {"data": json.dumps(data)}
 
         return render(request, 'sleeps/index.html', context)
     
