b7bcd67c16057bc418c8360dfd6dc2ec05819f50
Matt <matt.leach25@gmail.com>
2015-02-08 14:44:17 +0000

    Adding stacked view for stacked bar chart showing sleep time

--- beddit/sleeps/urls.py
+++ beddit/sleeps/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from sleeps import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.stacked, name='stacked'),
 ]
--- beddit/sleeps/views.py
+++ beddit/sleeps/views.py
@@ -5,7 +5,10 @@ from django.conf import settings
 import json
 from datetime import datetime
 
+import datetime
+
 from core.decorators import requires_token
+
 
 def convert_date_format(date_string):
     """
@@ -13,11 +16,19 @@ def convert_date_format(date_string):
     """
     return datetime.strptime(date_string, '%Y-%m-%d').strftime('%d/%m')
 
+def convert_epoch_to_time_after_3pm(secs):
+    t = datetime.datetime.fromtimestamp(secs)
+    from_3 = t - t.replace(hour=15, minute=0, second=0)
+
+    
+    return from_3.seconds / 3600.0 # Return in hours
+
+
 @requires_token
 def index(request):
             
     b = BedditClient(api_endpoint=settings.BEDDIT_API, token=request.session["token"], user_id=request.user.id)
-            
+    
     sleep_data = b.get_sleep_scores()
     
     data = [{"date": convert_date_format(date), "score": score} for date, score in sleep_data.items()]
@@ -26,3 +37,46 @@ def index(request):
 
     return render(request, 'sleeps/index.html', context)
     
+
+@requires_token
+def stacked(request):
+    # Not recording, Away, asleep, Awake, Break
+    
+    
+    b = BedditClient(api_endpoint=settings.BEDDIT_API, token=request.session["token"], user_id=request.user.id)
+    
+    sleeps = b.requestor.get_all_sleeps("2000-01-01", "2020-01-01")
+    
+    data = []
+    for sleep in sleeps.json():
+        date = sleep['date']
+        zones = sleep["time_value_tracks"]["sleep_stages"]["items"]
+        zones.append([sleep["end_timestamp"], 0])
+        
+        new_zones = []
+        
+        
+        y0 = 0
+        name = 0
+        for z in zones:
+            y1 = convert_epoch_to_time_after_3pm(z[0])
+            
+            new_zones.append({"name": name, "y0": y0, "y1": y1})
+            y0 = y1
+            name = z[1]
+        
+        new_zones.append({"name": 0, "y0": y0, "y1": 24})
+        
+        data.append({"date": date, "zones": new_zones})
+        
+        
+        
+        
+    return render(request, 'sleeps/stacked.html', {"data": json.dumps(data)})
+
+
+
+
+
+
+
